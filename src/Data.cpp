cceeea3257e478d4669ea5c4ec19dc8f392d5429
Nicos44k <nicolas141299@gmail.com>
2020-12-03 11:57:34 +0100

    [feat] add data method cpp

--- src/Data.cpp
+++ src/Data.cpp
@@ -1,1 +1,28 @@
-#include "../inc/Data.h"
+#include "../inc/Data.h"Data::Data(){
+Data::Data(){
+    // TODO : Constructor method
+}
+
+Data::~Data(){
+    // TODO : Destructor method
+}
+
+Data::add(){
+    // TODO : Add sample into data array
+}
+
+Data::aspire_les_donnees(){
+    // TODO : Function to define
+}
+
+Data::load(){
+    // TODO : Function to define
+}
+
+int Data::nbSamples(){
+    // TODO : Returns _nb_samples
+}
+
+char* Data::toString(){
+    // TODO : Give str representation
+}
