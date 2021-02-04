989d0ac83610c86190a941b48451f64927f0370e
Pierre NICOLAS <nicolas141299@gmail.com>
2021-02-04 22:31:55 +0100

    [refactor] update scalar product calcul

--- src/FeatureVector.cpp
+++ src/FeatureVector.cpp
@@ -24,14 +24,11 @@ int FeatureVector::getSize() {
 }
 
 double FeatureVector::operator*(FeatureVector B) {
+    // a scalar b = sum(a[i]*b[i]) with i = {0,n}
     FeatureVector A = *this;
-
-    vector<double> vector;
+    double r = 0;
     for (int i = 0; i < A.getSize(); i++) {
-        vector.push_back(A[i] + B[i]);
+        r += A[i] * B[i];
     }
-    FeatureVector AB(&vector);
-
-    // Scalar product of A and B : (|A+B|² - |A|² - |B|²) / 2
-    return (pow(AB.getNorme(), 2) - pow(A.getNorme(), 2) - pow(B.getNorme(), 2)) / 2;
+    return r;
 }
