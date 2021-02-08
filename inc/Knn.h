986455bf4ed3ac18de5a05c8c27e35c3f09bb689
Nurb44 <72301821+Nurb44@users.noreply.github.com>
2021-02-08 13:32:14 +0100

    [feat] Knn

--- inc/Knn.h
+++ inc/Knn.h
@@ -1,6 +1,19 @@
 #ifndef KNN_H
 #define KNN_H
 
+#include <iostream>
+#include <vector>
+#include "Data.h"
 
+class Knn
+{
+    public:
+    Knn();
+    int predict(Data d, FeatureVector f, const unsigned int k);
+
+    protected:
+    virtual int* predictSingle(unsigned int k, std::vector<double> cos, Data d);
+    virtual std::vector<double> similarity(Data d, FeatureVector f);
+};
 
 #endif
--- src/Knn.cpp
+++ src/Knn.cpp
@@ -1,1 +1,30 @@
 #include "../inc/Knn.h"
+
+Knn::Knn()
+{}
+
+int Knn::predict(Data d, FeatureVector f, const unsigned int k)
+{
+    // Calculation of cosinus similarity
+    std::vector<double> cos = similarity(d, f);
+
+    // k associated tag
+    int *tag = predictSingle(k, cos, d);
+
+    // Choose tag
+    unsigned int nbTag[10];
+    for(unsigned int i = 0; i < k; i++)
+    {
+        nbTag[tag[i]]++;
+    }
+    int tagMax = 0;
+    for(unsigned int i = 0; i < k - 1; i++)
+    {
+        if(nbTag[i] < nbTag[i + 1])
+        {
+            tagMax = i + 1;
+        }
+    }
+
+    return tagMax;
+}
