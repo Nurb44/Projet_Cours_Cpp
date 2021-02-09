dc9be0dbc90c536a145fde022a07d78c94de6f59
Nurb44 <72301821+Nurb44@users.noreply.github.com>
2021-02-09 17:13:39 +0100

    [refactor] update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,13 @@
 #include <iostream>
 #include <stdlib.h>
-#include <chrono>
 #include <vector>
 #include "..\inc\Command.h"
+#include "..\inc\Timer.h"
 #include "..\inc\Data.h"
 #include "..\inc\KnnCosine.h"
 #include "..\inc\ClassificationReport.h"
 
 using namespace std;
-
-class Timer
-{
-public:
-    Timer() : beg_(clock_::now()) {}
-    void reset() { beg_ = clock_::now(); }
-    double elapsed() const {
-        return std::chrono::duration_cast<second_>
-                (clock_::now() - beg_).count(); }
-
-private:
-    typedef std::chrono::high_resolution_clock clock_;
-    typedef std::chrono::duration<double, std::ratio<1> > second_;
-    std::chrono::time_point<clock_> beg_;
-};
 
 int main()
 {
@@ -38,21 +23,20 @@ int main()
         bool process = false;
         // Declaration of data
         Data trainingData;
-        Data *testData;
+        Data *testData = new Data();
         // Load for training data
         if(trainingData.load_from_svm(cmd.getTrainingDataPath()))
         {
             // If value then split for test data
-            if(cmd.getTestDataPathValue())
+            if(cmd.getTestDataPathStatus())
             {
                 delete testData;
-                testData = trainingData.split(stoi(cmd.getTestDataPath()));
+                testData = trainingData.split(cmd.getTestDataPathValue());
                 process = true;
             }
             // If path then load for test data
             else
             {
-                testData = new Data();
                 if(testData->load_from_svm(cmd.getTestDataPath()))
                 {
                     process = true;
@@ -105,8 +89,7 @@ int main()
                     }
 
                     // Stop and display timer
-                    double t = tmr.elapsed();
-                    cout << "Time to work: " << t << endl;
+                    cout << tmr.getString() << endl;
                 }
                 else
                 {
